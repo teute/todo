b3290d39a660c57c8bbd8c3e39e1c22364039891
Salieu Kamara <kamaras@ecm15-mac-17.coventry.ac.uk>
2016-10-20 09:47:29 +0100

    Initial Commit

---
+++ ListMaker/ListMaker.h
@@ -0,0 +1,19 @@
+//
+//  ListMaker.h
+//  ListMaker
+//
+//  Created by Salieu Kamara on 20/10/2016.
+//  Copyright © 2016 Coventry University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ListMaker.
+FOUNDATION_EXPORT double ListMakerVersionNumber;
+
+//! Project version string for ListMaker.
+FOUNDATION_EXPORT const unsigned char ListMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListMaker/PublicHeader.h>
+
+
